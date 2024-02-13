1d2169af6dd1b27da8d58fdf5ff33cd93621466d
efratdonat <efratdonat@gmail.com>
2024-02-13 19:56:51 +0200

    123

--- server/DB/mongodb/connectToAtlas.js
+++ server/DB/mongodb/connectToAtlas.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const config = require("config");
 
-const userName = config.get("DB_NAME");
-const password = config.get("DB_PASSWORD");
+const userName = config.get("efratdonat");
+const password = config.get("Ed3247668");
 
 mongoose
   .connect(
-    `mongodb://localhost:27017/${userName}:${password}@efrat_firstand`
+    `mongodb+srv://efratdonat:Ed3247668@efrat.kxf2wzw.mongodb.net/`
   )
-  .then(() => console.log(chalk.magentaBright("connected to MongoDb Locally!")))
+  .then(() => console.log(chalk.magentaBright("connected to MongoDb cloud!")))
   .catch((error) =>
     console.log(chalk.redBright.bold(`could not connect to mongoDb: ${error}`))
   );
